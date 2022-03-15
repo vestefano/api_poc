eb7dd7b25fda9334fcd3410c8e6ca0af5cc40d2b
vestefano <vestefanor@gmail.com>
2022-03-15 15:02:39 -0400

    Change the url order

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,14 +5,14 @@ from accounts import views
 
 urlpatterns = [
     path('user/', views.ListUserApiView.as_view(), name='account_list'),
-    path('user/<int:pk>/', views.RetrieveUserApiView.as_view(), name='account_details'),
     path('user/create/', views.CreateUserApiView.as_view(), name='account_create'),
+    path('user/<int:pk>/', views.RetrieveUserApiView.as_view(), name='account_details'),
     path('user/update/<int:pk>/', views.UpdateUserApiView.as_view(), name='account_update'),
     path('user/delete/<int:pk>/', views.DestroyUserApiView.as_view(), name='account_delete'),
 
     path('profile/', views.ListProfileApiView.as_view(), name='profile_list'),
-    path('profile/<int:user_id>/', views.RetrieveProfileApiView.as_view(), name='profile_details'),
     path('profile/create/', views.CreateProfileApiView.as_view(), name='profile_create'),
+    path('profile/<int:user_id>/', views.RetrieveProfileApiView.as_view(), name='profile_details'),
     path('profile/update/<int:pk>/', views.UpdateProfileApiView.as_view(), name='profile_update'),
     path('profile/delete/<int:pk>/', views.DestroyProfileApiView.as_view(), name='profile_delete'),
 
