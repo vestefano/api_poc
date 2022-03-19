20c2458257bb2129c3bb54c7512cd2f2f1d8cc66
vestefano <vestefanor@gmail.com>
2022-03-19 18:43:03 -0400

    Pylint fixes accounts/app was refactored.

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,8 @@
+"""Accounts apps"""
 from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """AccountsConfig class"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
