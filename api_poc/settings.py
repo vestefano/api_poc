5b994ef3ff54c5819cb30237ed8ee055909d5ca0
vestefano <vestefanor@gmail.com>
2022-03-18 20:15:33 -0400

    Debug False

--- api_poc/settings.py
+++ api_poc/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2bzrqh=b&r4elhmft#m5i8r-4#ga-m)(tn#5!dlr_^hc*-te!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
